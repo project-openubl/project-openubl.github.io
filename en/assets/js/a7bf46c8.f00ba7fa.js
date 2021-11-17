77e600fcaab07dfcb46ebc179f30298a57a70037
carlosthe19916 <carlosthe19916@users.noreply.github.com>
2021-11-17 17:17:30 +0000

    deploy: project-openubl/website@fbe4177ca6f9a19f58369b522fefa7216da9f4ef

--- en/assets/js/a7bf46c8.f00ba7fa.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproject_openubl=self.webpackChunkproject_openubl||[]).push([[8525],{95936:function(e){e.exports=JSON.parse('{"allTagsPath":"/en/blog/tags","slug":"/en/blog/tags/searchpe","name":"searchpe","count":2,"permalink":"/en/blog/tags/searchpe"}')}}]);
