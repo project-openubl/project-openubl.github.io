efb2e36dd69de84e354ccb8a7e0f58b6923c98ca
carlosthe19916 <carlosthe19916@users.noreply.github.com>
2022-05-14 10:27:12 +0000

    deploy: project-openubl/website@ee9d6882b2e0b11053a255ed952c150e4f5c28d1

--- assets/js/83d480e9.2ea289db.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproject_openubl=self.webpackChunkproject_openubl||[]).push([[205],{3672:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","name":"release","count":2,"permalink":"/blog/tags/release"}')}}]);
