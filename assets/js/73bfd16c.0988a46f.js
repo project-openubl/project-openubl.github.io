126e355e91b04d9011f3361220a0085c9399bb18
carlosthe19916 <carlosthe19916@users.noreply.github.com>
2023-01-13 08:53:20 +0000

    deploy: project-openubl/website@9e22d29c115e1b93088a0b89c4c554e8a54b8cd6

--- assets/js/73bfd16c.0988a46f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproject_openubl=self.webpackChunkproject_openubl||[]).push([[8674],{9450:e=>{e.exports=JSON.parse('{"label":"release","permalink":"/blog/tags/release","allTagsPath":"/blog/tags","count":16}')}}]);
--- assets/js/7ccdbf55.c33136a3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproject_openubl=self.webpackChunkproject_openubl||[]).push([[4423],{5342:e=>{e.exports=JSON.parse('{"permalink":"/blog/tags/ublhub","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
