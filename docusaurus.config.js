57361255b95ca408177d9857e039f95566650f3a
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2026-03-01 15:01:53 +0100

    upgrade packages
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -281,9 +281,7 @@ const config = {
       return {
         name: "docusaurus-tailwindcss",
         configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require("tailwindcss"));
-          postcssOptions.plugins.push(require("autoprefixer"));
+          postcssOptions.plugins.push(require("@tailwindcss/postcss"));
           return postcssOptions;
         },
       };
